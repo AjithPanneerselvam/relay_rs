4f89f30433bf3bc66c7bbda40f790e174da26e29
AjithPanneerselvam <ajithpanneerselvam20@gmail.com>
2020-03-26 23:38:11 +0530

    Handle brokenpipe error

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,39 @@
-use std::io::{self, Read, Write}; 
+use std::env;
+use std::io::{self, ErrorKind, Read, Result, Write};
 
-const CHUNK_SIZE: usize = 16 * 1024; 
+const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
-    let mut total_bytes = 0; 
+fn main() -> Result<()> {
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    let mut total_bytes = 0;
 
     loop {
-        let mut buffer = [0; CHUNK_SIZE]; 
+        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
-            Ok(0) => break, 
-            Ok(x) => x, 
-            Err(_) => break, 
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
         };
-        
-        total_bytes += num_read; 
-        io::stdout().write_all(&buffer[..num_read]).unwrap(); 
-    } 
 
-    eprintln!("total_bytes: {}", total_bytes); 
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        total_bytes += num_read;
+
+        if let Err(e) = io::stdout().write(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+
+            return Err(e);
+        }
+
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+
+    if !silent {
+        eprint!("\r{}", total_bytes);
+    }
+
+    Ok(())
 }
